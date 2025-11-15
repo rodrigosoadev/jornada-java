3032b89863bdaaecd5e3d20822705eb461e5e4e4
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-11-15 13:41:37 -0300

    att

--- arrays/introducaoArrays.java
+++ arrays/introducaoArrays.java
@@ -2,17 +2,17 @@ package arrays;
 
 public class introducaoArrays {
     public static void main(String[] args) {
-        double[] notas = new double[5];
-        notas[0] = 7;
-        notas[1] = 2;
-        notas[2] = 9;
-        notas[3] = 4;
-        notas[4] = 9;
+        // array sempre obj
+        double[] notas = new double[] {1,2,3,4};
+        //notas[0] = 7;
+        //notas[1] = 2;
+       // notas[2] = 9;
+        //notas[3] = 4;
         System.out.println("Tamanho do array: "+notas.length);
-        for (int i = 0; i < notas.length; i++) {
-            System.out.println(notas[i]);
+        for (double aux : notas) {
+            System.out.println(aux);
         }
-
         
     }
+    
 }
