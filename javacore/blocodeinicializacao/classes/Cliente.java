6db0171aa992c2e5d23cd15aaa23c3c0359fee3c
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-01 19:21:06 -0300

    Blocos de inicialização

---
+++ javacore/blocodeinicializacao/classes/Cliente.java
@@ -0,0 +1,34 @@
+package javacore.blocodeinicializacao.classes;
+    //1 - alocado espaço na memoria para o obj que será criado
+    // 2 - cada atributo de classe é criado e inicializado com seus valores default ou valores explicitos
+    // 3 - Bloco de inicialização é executado
+    // 4 - o construtor é executado
+
+public class Cliente {
+    
+    private int[] parcelas;
+    {
+        parcelas = new int[100];
+        System.out.println("Exibindo quantidade de parcelas possiveís");
+        for (int i = 1; i < 100; i++) {
+            parcelas[i-1] = i;
+        }
+    }
+
+    public Cliente() {
+        
+    }
+
+    //metodo inicial
+    public int[] getParcelas() {
+        return parcelas;
+    }
+
+    public void setParcelas(int[] parcelas) {
+        this.parcelas = parcelas;
+    }
+    //metodo final
+    
+
+    
+}
---
+++ javacore/blocodeinicializacao/test/ClienteTest.java
@@ -0,0 +1,12 @@
+package javacore.blocodeinicializacao.test;
+
+import javacore.blocodeinicializacao.classes.Cliente;
+
+public class ClienteTest {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        
+    }
+}
+
+
