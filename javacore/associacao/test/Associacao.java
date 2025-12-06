6e237700930ce0ead004d890af166461334792e7
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-05 21:32:36 -0300

    test - não foi nada alterado no código para aula de hoje

--- javacore/associacao/test/Associacao.java
+++ javacore/associacao/test/Associacao.java
@@ -9,6 +9,7 @@ public class Associacao {
     public static void main(String[] args) {
         Aluno aluno = new Aluno("Rodrigo Soares", 24);
         Aluno aluno2 = new Aluno("Gabriel Caleb", 14);
+        Aluno aluno3 = new Aluno("Ricardo", 36);
 
         Seminario seminario = new Seminario("Como ser um the power, the best programador");
         Professor prof = new Professor("Yoda","Usar a força para programar");
@@ -17,11 +18,11 @@ public class Associacao {
 
         aluno.setSeminario(seminario);
         aluno2.setSeminario(seminario);
-        System.out.println("----------------");
+        System.out.println("---------------");
         
         seminario.setProfessor(prof);
         seminario.setLocal(local);
-        seminario.setAlunos(new Aluno[]{aluno,aluno2});
+        seminario.setAlunos(new Aluno[]{aluno,aluno2, aluno3});
 
         Seminario[] seminariosArray = new Seminario[1];
         seminariosArray[0] = seminario;
