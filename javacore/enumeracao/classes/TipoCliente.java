ee5a395db2910451bc4500a4a5ace34f8330b9d4
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-09 19:53:05 -0300

    Enumeração pt 01

---
+++ javacore/enumeracao/classes/TipoCliente.java
@@ -0,0 +1,5 @@
+package javacore.enumeracao.classes;
+
+public enum TipoCliente {
+    PESSOA_FISICA, PESSOA_JURIDICA
+}
---
+++ javacore/enumeracao/test/ClienteTest.java
@@ -0,0 +1,12 @@
+package javacore.enumeracao.test;
+
+import javacore.enumeracao.classes.Cliente;
+import javacore.enumeracao.classes.TipoCliente;
+
+public class ClienteTest {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Rodrigo", TipoCliente.PESSOA_FISICA);
+
+        System.out.println(cliente);
+    }
+}
