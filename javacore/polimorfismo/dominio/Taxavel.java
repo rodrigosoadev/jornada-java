90630f5b43ffc626283825704c151dfee9a01ff6
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-11 19:38:04 -0300

    Polimorfismo pt 01 - Introdução

---
+++ javacore/polimorfismo/dominio/Taxavel.java
@@ -0,0 +1,5 @@
+package javacore.polimorfismo.dominio;
+
+public interface Taxavel {
+    public abstract double calcularImposto();
+}
---
+++ javacore/polimorfismo/test/ProdutoTest01.java
@@ -0,0 +1,16 @@
+package javacore.polimorfismo.test;
+
+import javacore.polimorfismo.dominio.Computador;
+import javacore.polimorfismo.dominio.Tomate;
+import javacore.polimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest01 {
+    public static void main(String[] args) {
+        Computador c = new Computador("NUC10I7", 9000);
+        Tomate t = new Tomate("Tomate Italiano", 10);
+        CalculadoraImposto.calcularImpostoComputador(c);
+        System.out.println("------------------------");
+        CalculadoraImposto.calcularImpostoTomate(t);
+
+    }
+}
