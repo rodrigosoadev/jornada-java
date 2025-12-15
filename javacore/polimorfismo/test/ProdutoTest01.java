17880a17c1ceab68432549b313ee8a51ba24dcd3
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-15 19:12:35 -0300

    Polimorfismo pt 03 - Parâmetros polimórficos

--- javacore/polimorfismo/test/ProdutoTest01.java
+++ javacore/polimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package javacore.polimorfismo.test;
 
 import javacore.polimorfismo.dominio.Computador;
+import javacore.polimorfismo.dominio.Televisao;
 import javacore.polimorfismo.dominio.Tomate;
 import javacore.polimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,14 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador c = new Computador("NUC10I7", 9000);
         Tomate t = new Tomate("Tomate Italiano", 10);
-        CalculadoraImposto.calcularImpostoComputador(c);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
+
+
+        CalculadoraImposto.caulcularImposto(c);
         System.out.println("-----------------------");
-        CalculadoraImposto.calcularImpostoTomate(t);
+        CalculadoraImposto.caulcularImposto(t);
+        System.out.println("-----------------------");
+        CalculadoraImposto.caulcularImposto(tv);
 
     }
 }
