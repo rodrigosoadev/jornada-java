6add191f9c208fed533a8ed342453db367079e18
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-18 20:41:07 -0300

    Classes Utilitárias - Wrappers pt 01

---
+++ javacore/Cintermediario/Bwrappers/test/WrapperTest01.java
@@ -0,0 +1,26 @@
+package javacore.Cintermediario.Bwrappers.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longp = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'R';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'R';
+        Boolean booleanW = false;
+
+
+    }
+
+}
