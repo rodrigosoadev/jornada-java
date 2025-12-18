0ec88ba08c3c1a1ec4a80651a3cb42355f05b324
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-18 19:11:34 -0300

    Exceções pt 09 - Try with resources

---
+++ javacore/Cintermediario/excecoes/exception/dominio/Leitor1.java
@@ -0,0 +1,14 @@
+package javacore.Cintermediario.excecoes.exception.dominio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Leitor1 implements Closeable{
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Fechando leitor 1");
+        
+    }
+    
+}
