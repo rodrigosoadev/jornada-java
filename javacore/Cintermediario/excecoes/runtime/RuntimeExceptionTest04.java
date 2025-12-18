e3658f1c723c0b3bd2b6a1713b62d9388fab4875
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-18 18:31:59 -0300

    Exceções pt 07 - Capturando múltiplas exceções

---
+++ javacore/Cintermediario/excecoes/runtime/RuntimeExceptionTest04.java
@@ -0,0 +1,35 @@
+package javacore.Cintermediario.excecoes.runtime;
+//excessões mais genericas tem que ficar no final
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro do IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do ArithmeticException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+
+        try {
+            talvezLanceException();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+
+    }
+
+}
