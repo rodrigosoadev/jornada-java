abaa2ee01db337a4ffba3e2fc38b13ee35d7a4d5
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-23 19:19:03 -0300

    Classes Utilitárias - Internacionalização de moeda com Locale. Por algum motivo que ainda não identifquei, está ocorrendo erro  de Unparseable number, eu acredito que seja por motivo do Locale está ficando em desuso

--- javacore/Cintermediario/Eformatacao/test/NumberFormatTest02.java
+++ javacore/Cintermediario/Eformatacao/test/NumberFormatTest02.java
@@ -4,27 +4,29 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
+public class NumberFormatTest02 {
     public static void main(String[] args) {
-        Locale localePT = new Locale("pt", "BR");
+        Locale localeBR = new Locale("pt", "BR");
         Locale localeJP = Locale.JAPAN;
         Locale localeIT = Locale.ITALY;
         NumberFormat[] nfa = new NumberFormat[4];
-        nfa[0] = NumberFormat.getInstance();
-        nfa[1] = NumberFormat.getInstance(localeJP);
-        nfa[2] = NumberFormat.getInstance(localePT);
-        nfa[3] = NumberFormat.getInstance(localeIT);
-        double valor = 10_000.2130;
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+        double valor = 1000.2130;
         for (NumberFormat numberFormat : nfa) {
             System.out.println(numberFormat.getMaximumFractionDigits());
-            numberFormat.setMaximumFractionDigits(2);
             System.out.println(numberFormat.format(valor));
         }
-        String valoString = "1_000.2130";
+
+        String valorString = "1,000";
         try {
-            System.out.println(nfa[0].parse(valoString));
+            System.out.println(nfa[1].parse(valorString));
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
+
     }
 }
