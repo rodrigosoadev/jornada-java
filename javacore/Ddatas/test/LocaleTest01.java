f2b9e73cfdfdfae73824d1ffd71a2a7dfabca289
Rodrigo <meuemail.rodrigodev@gmail.com>
2025-12-23 18:31:14 -0300

    Classes Utilitárias - Internacionalização Datas com Locale

---
+++ javacore/Ddatas/test/LocaleTest01.java
@@ -0,0 +1,24 @@
+package javacore.Ddatas.test;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeCH = new Locale("it", "CH");
+
+        Calendar calendar = Calendar.getInstance();
+
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+        System.out.println("Italia " + df1.format(calendar.getTime()));
+        System.out.println("Suiça " + df2.format(calendar.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry());
+        System.out.println(localeCH.getDisplayCountry());
+
+    }
+}
