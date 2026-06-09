ac8a38be908afb4180e205e0f3964fc67fe40642
Rodrigo <meuemail.rodrigodev@gmail.com>
2026-06-09 18:37:48 -0300

    Classes Utilitárias - NIO pt 06 - BasicFileAttributes pt 01

---
+++ src/javacore/Cintermediario/Inio.test/BasicFileAttibutesTest01.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.nio.file.Path;
+
+public class BasicFileAttibutesTest01 {
+    public static void main(String[] args) throws IOException{
+        //BasicAttributes, DosFileAttributes, PosixFileAttributes
+        LocalDateTime date = LocalDateTime.now().minusDays(10);
+        File file = new File("pasta2/novo.txt");
+        boolean isCreated = file.createNewFile();
+        boolean isModified = file.setLastModified(date.toInstant(ZoneOffset.UTC).toEpochMilli());
+        
+
+        Path path = Paths.get("pasta2/novo_path.txt");
+        Files.createFile(path);
+        FileTime fileTime = FileTime.from(date.toInstant(ZoneOffset.UTC));
+        Files.setLastModifiedTime(path,fileTime);
+        System.out.println(Files.isWritable(path));
+        System.out.println(Files.isReadable(path));
+        System.out.println(Files.isExecutable(path));
+    }
+}
