6f179bf7ab60168ab454570528d7bc27390d2522
Rodrigo <meuemail.rodrigodev@gmail.com>
2026-07-07 11:22:29 -0300

    Classes Utilitárias - NIO pt 14 - PathMatcher pt 02

---
+++ src/javacore/Cintermediario/Inio.test/PathMatcherTest02.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.PathMatcher;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+
+class FindAllTestJavaOrClass extends SimpleFileVisitor<Path>{
+    private PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*{Test*}.{java,class}");
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (matcher.matches(file)) {
+            System.out.println(file.getFileName());
+
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class PathMatcherTest02 {
+    public static void main(String[] args)  throws IOException{
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new FindAllTestJavaOrClass());
+    }
+}
