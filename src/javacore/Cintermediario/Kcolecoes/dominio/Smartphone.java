033c638c974b764d0dff0acb96b05821af621472
Rodrigo <meuemail.rodrigodev@gmail.com>
2026-07-08 11:05:08 -0300

    Coleções pt 02 - equals pt 02

--- src/javacore/Cintermediario/Kcolecoes/dominio/Smartphone.java
+++ src/javacore/Cintermediario/Kcolecoes/dominio/Smartphone.java
@@ -9,6 +9,24 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    
+    //tem que ser reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    //Simetrico: para x e y diferentes de null, se x.quals(y) == true logo, y.equals(x) == true
+    //Transitividade: para x, y, z diferentes de null, se x.quals(y) == true, logo y.equals(x) == true, e x.equals(z) == true logo, y.quals(z) == true
+    //Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    //para x diferente de null, x.equals(null) tem que retornar false
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null) return false;
+        if(this == obj) return true;
+        if(this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+        
+    }
+
+
+
     public String getSerialNumber() {
         return serialNumber;
     }
--- src/javacore/Cintermediario/Kcolecoes/test/EqualsTest01.java
+++ src/javacore/Cintermediario/Kcolecoes/test/EqualsTest01.java
@@ -5,7 +5,7 @@ import javacore.Cintermediario.Kcolecoes.dominio.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1", "iphone");
-        Smartphone s2 = new Smartphone("1ABC1", "iphone");
+        Smartphone s2 = new Smartphone("1ABC1", "Pixel");
         System.out.println(s1.equals(s2));
     }
 }
