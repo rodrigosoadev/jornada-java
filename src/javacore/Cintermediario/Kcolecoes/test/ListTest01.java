d981f1e9d1961d048b783449a0e63268659571a7
Rodrigo <meuemail.rodrigodev@gmail.com>
2026-07-09 10:29:15 -0300

    Coleções pt 07 - List pt 02

--- src/javacore/Cintermediario/Kcolecoes/test/ListTest01.java
+++ src/javacore/Cintermediario/Kcolecoes/test/ListTest01.java
@@ -6,18 +6,29 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>();
+        List<String> nomes2 = new ArrayList<>();
         nomes.add("Rodrigo");
         nomes.add("DevDojo");
-        
+        nomes2.add("Soares");
+        nomes2.add("Camara");
+
+        nomes.addAll(nomes2);
 
         for(String nome : nomes){
             System.out.println(nome);
         }
-        nomes.add("Soares");
+        
         System.out.println("---------");
-        for(int i = 0; i < nomes.size(); i++){
+
+        int size = nomes.size();
+        for(int i = 0; i < size; i++){
+            nomes.add("Soares");
             System.out.println(nomes.get(i));
         }
+        
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+       
 
     }
 }
