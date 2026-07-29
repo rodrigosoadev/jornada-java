5921868bc625043abef8c04279091cecd842a73e
Rodrigo <meuemail.rodrigodev@gmail.com>
2026-07-29 16:34:17 -0300

    Generics pt 05 - Classes Genéricas pt 02

---
+++ src/javacore/Cintermediario/Lgenerics/service/RentalService.java
@@ -0,0 +1,30 @@
+package javacore.Cintermediario.Lgenerics.service;
+
+import javacore.Cintermediario.Lgenerics.dominio.Carro;
+import java.util.List;
+
+public class RentalService<T> {
+
+    private List<T> objetosDisponiveis;
+
+    public RentalService(List<T> objetosDisponiveis) {
+        this.objetosDisponiveis = objetosDisponiveis;
+    }
+
+    public T buscarObjetoDisponivel() {
+
+        System.out.println("Buscando objeto disponivel...");
+        T t = objetosDisponiveis.remove(0);
+        System.out.println("Alugando objeto: " + t);
+        System.out.println("Objetos disponiveis para alugar: ");
+        System.out.println(objetosDisponiveis);
+        return t;
+    }
+
+    public void retornarObjetoAlugado(T t) {
+        System.out.println("Devolvendo carro: " + t);
+        objetosDisponiveis.add(t);
+        System.out.println("Objetos disponiveis para alugar: ");
+        System.out.println(objetosDisponiveis);
+    }
+}
---
+++ src/javacore/Cintermediario/Lgenerics/test/ClasseGenericaTest03.java
@@ -0,0 +1,32 @@
+package javacore.Cintermediario.Lgenerics.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javacore.Cintermediario.Lgenerics.dominio.Barco;
+import javacore.Cintermediario.Lgenerics.dominio.Carro;
+import javacore.Cintermediario.Lgenerics.service.BarcoRentavelService;
+import javacore.Cintermediario.Lgenerics.service.CarroRentavelService;
+import javacore.Cintermediario.Lgenerics.service.RentalService;
+
+public class ClasseGenericaTest03 {
+    public static void main(String[] args) {
+        List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW"), new Carro("Fusca")));
+
+        List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Canoa")));
+
+        RentalService<Carro> rentalServiceCarro = new RentalService<>(carrosDisponiveis);
+
+        Carro carro = rentalServiceCarro.buscarObjetoDisponivel();
+        System.out.println("Usando o carro por um mês...");
+        rentalServiceCarro.retornarObjetoAlugado(carro);
+
+        System.out.println("---------------------");
+        RentalService<Barco> rentalServiceBarco = new RentalService<>(barcosDisponiveis);
+
+        Barco barco = rentalServiceBarco.buscarObjetoDisponivel();
+        System.out.println("Usando o barco por um mês...");
+        rentalServiceBarco.retornarObjetoAlugado(barco);
+
+    }
+}
