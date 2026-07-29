484e5e599c3726ee539f70c54132b15a951bf1d7
Rodrigo <meuemail.rodrigodev@gmail.com>
2026-07-29 16:49:22 -0300

    Generics pt 06 - Métodos Genéricos

---
+++ src/javacore/Cintermediario/Lgenerics/test/MetodoGenericoTest01.java
@@ -0,0 +1,22 @@
+package javacore.Cintermediario.Lgenerics.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javacore.Cintermediario.Lgenerics.dominio.Barco;
+
+public class MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Barco> barcoList = criarArrayComUmObjeto(new Barco("Canoa"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrayComUmObjeto(T t) {
+        return List.of(t);
+    }
+
+    
+
+}
+
+
